2f567bac17d7925b3370e1f179428370335db144
Emily Asimbaya <a_asimbaya@hotmail.com>
2023-11-12 10:46:35 -0500

    declaracion de funciones, variables y librerias

---
+++ funciones.h
@@ -0,0 +1,16 @@
+//Contiene declaraciones de variables y funciones//
+#ifndef FUNCIONESH
+#define FUNCIONESH
+#include <stdio.h>
+#include <stdlib.h>
+//Declarando funciones 
+int suma(int a, int b);
+int resta(int a, int b);
+int multiplicacion(int a, int b);
+int division(int a, int b);
+
+//Declarando variables
+
+int resultado;
+#endif
+
